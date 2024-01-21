d42767eaefdbf63e94a165acb86eb2daf25d096b
deku <135095586+deku1824@users.noreply.github.com>
2024-01-22 05:25:45 +1200

    Add files via upload

---
+++ loginsignup/user_page.php
@@ -0,0 +1,34 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>user page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<h1>Loginin success</h1>
+      <a href="logout.php" class="btn">logout</a>
+
+
+</div>
+
+</body>
+</html>
